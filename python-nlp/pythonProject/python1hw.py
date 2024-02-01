282adda17b95a500eda4536c214c0794f83a2db9
charlie vazquez <cbv5172@psu.edu>
2024-02-01 17:27:48 -0500

    pushed a branch testing txt file to the alt branch in docs

--- python-nlp/pythonProject/python1hw.py
+++ python-nlp/pythonProject/python1hw.py
@@ -1,12 +1,7 @@
 import spacy
-spacy.prefer_gpu()
-
 nlp = spacy.load('en_core_web_sm')
 
-if __name__ == '__main__':
-    with open("char-chatgptex2_charlie-branch.txt", "r") as f:
-        contents = f.read()
-
-        nlp()
-
-#why isnt any of this being processed? it used to show the entire text in the box below
+spunchbop = open('spongebobsmth.txt', 'r', encoding='utf8')
+words = avatarSpeeches.read()
+wordstrings = str(words)
+print(wordstrings)

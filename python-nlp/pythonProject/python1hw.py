51628de7121cdb9b3cbcfd97301e52393e619747
charlie vazquez <cbv5172@psu.edu>
2024-02-01 19:45:06 -0500

    added some changes to the 1st python hw

--- python-nlp/pythonProject/python1hw.py
+++ python-nlp/pythonProject/python1hw.py
@@ -1,7 +1,16 @@
 import spacy
 nlp = spacy.load('en_core_web_sm')
 
-spunchbop = open('spongebobsmth.txt', 'r', encoding='utf8')
-words = avatarSpeeches.read()
+spunchbop = open('spoinglebop.txt', 'r', encoding='utf8') #what exactly is this thing that encoding equals supposed to be?
+words = spunchbop.read()
 wordstrings = str(words)
 print(wordstrings)
+
+oceanlyrics = nlp(wordstrings) #is the wordstrings in the parenthesis linked to a bank of features that nlp does?
+for token in oceanlyrics:
+    # if token.pos_ == "VERB":
+    print(token.text, "---->", token.pos_, ":::::", token.lemma_, token.shape_, ";;;;;;;") #what do the arrows and semicolons do? I assumed they were to accuratly find the function they were associated with but I think I ended up breaking it by adding my own bits
+
+    #ok this works now finally. my bad for stealing some code but I really had a tough time figuring out how this worked
+
+

8a27ad2eca402684241e9cb1478f317313be9d42
charlie vazquez <cbv5172@psu.edu>
2024-02-02 21:55:00 -0500

    finalized some things in python ex 2

--- python-nlp/pythonProject/python1hw.py
+++ python-nlp/pythonProject/python1hw.py
@@ -9,8 +9,9 @@ print(wordstrings)
 oceanlyrics = nlp(wordstrings) #is the wordstrings in the parenthesis linked to a bank of features that nlp does?
 for token in oceanlyrics:
     # if token.pos_ == "VERB":
-    print(token.text, "---->", token.pos_, ":::::", token.lemma_, token.shape_, ";;;;;;;") #what do the arrows and semicolons do? I assumed they were to accuratly find the function they were associated with but I think I ended up breaking it by adding my own bits
+    print(token.text, "---->", token.pos_, ":::::", token.lemma_, token.is_alpha, token.is_alpha, token.tag_) #what do the arrows and semicolons do? I assumed they were to accuratly find the function they were associated with but I think I ended up breaking it by adding my own bits
 
     #ok this works now finally. my bad for stealing some code but I really had a tough time figuring out how this worked
 
 
+23
